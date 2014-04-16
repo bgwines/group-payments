8e4a88e6e0afa7617fa431e190644584b3894b2c
Brett Wines <brettwines@gmail.com>
2014-04-16 01:09:42 -0700

    Works with file-reading!

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ group-payments
 ==============
 
 A script to simplify people paying each other for items in groups.
+
+How to use
+----------
+
+Run as follows
+
+    $ runhaskell payments.hs < input.txt
+
+Input file format
+-----------------
+
+You need an input file where each line conforms to the following format (where the '`<`' and '`>`' aren't in your file; they're just delimiters for purposes of describing input):
+
+    <item_name> <payer_name> <list_of_recipients_of_item> <cost>
+
+Each segment (e.g. <payer name>) can have no spaces, and <cost> must be a number. Additionally, <list of recipients of item> must be of the following format:
+
+    [<name_0>,<name_1>,...,<name_n>]
+
+, where there are *no spaces* after the commas, and the '`[`' and '`]`' are present.
+
+See `input.txt` for an example.
--- payments.hs
+++ payments.hs
@@ -1,4 +1,5 @@
 
+import qualified Data.List.Split as S
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Ord as O
@@ -10,32 +11,28 @@ from_just :: Maybe a -> a
 from_just (Just x) = x
 
 -- (item name, buyer name, recipients, cost)
-src_data :: [(String, String, [String], Double)]
-src_data = [
-	("TV",                "Tom",   ["Tom", "Lucas", "Brett"], 1000),
-	("Rug",               "Brett", ["Tom", "Lucas", "Brett"], 300),
-	("Swedish Fish",      "Lucas", ["Brett"],                 3),
-	("Laundry Detergent", "Lucas", ["Lucas", "Tom"],          10)]
+src_data :: String
+src_data =
+	"TV                Tom   [Tom,Lucas,Brett] 1000\n" ++
+	"Rug               Brett [Tom,Lucas,Brett] 300\n" ++
+	"SwedishFish       Lucas [Brett]           3\n" ++
+	"LaundryDetergent  Lucas [Lucas,Tom]       10"
 
-src_data' :: [(String, [String], Double)]
-src_data' = map (\(_, b, c, d) -> (b, c, d)) src_data
-
-who_owes_whom_how_much :: [(String, String, Double)]
+who_owes_whom_how_much :: [(String, [String], Double)] -> [(String, String, Double)]
 who_owes_whom_how_much =
-	filter h . map g . L.group . L.sort . concat . map f $ src_data'
+	filter h . map g . L.group . L.sort . concat . map f
 	where
 		f (buyer, owers, amount) =
 			[(ower, buyer, amount / (length'' owers)) | ower <- owers]
 		g = foldl1 (\(a, b, r) (a', b', r') -> (a, b, r + r'))
 		h (a, b, _) = a /= b
 
-who_owes_how_much :: [(String, Double)]
+who_owes_how_much :: [(String, String, Double)] -> [(String, Double)]
 who_owes_how_much =
 	reverse
 		. L.sortBy (O.comparing snd)
 			. M.toList
 				. foldl update_record M.empty
-					$ who_owes_whom_how_much
 	where update_record m record@(a, b, r) = m''
 		where
 			s = M.lookup a m
@@ -50,3 +47,24 @@ who_owes_how_much =
 
 			m'  = M.insert a s'' m
 			m'' = M.insert b t'' m'
+
+calc_optimal_payment_strategy :: [(String, [String], Double)] -> M.Map String (String, Double)
+calc_optimal_payment_strategy formatted_input = M.fromList as_list
+	where
+		l = who_owes_how_much $ who_owes_whom_how_much $ formatted_input
+		as_list = zipWith (\(a, e) (b, _) -> (a, (b, e))) l (tail l)
+
+read' :: String -> Double
+read' d = read d :: Double
+
+format :: String -> [(String, [String], Double)]
+format = map format_line . lines
+	where
+		format_line = f . to_tuple4 . words
+		to_tuple4 l = (l !! 0, l !! 1, l !! 2, l !! 3)
+		f (a, b, c, d) = (b, g c, read' d)
+		g = S.splitOn "," . tail . init
+
+main = do
+	contents <- getContents
+	putStrLn . show $ calc_optimal_payment_strategy (format contents)
